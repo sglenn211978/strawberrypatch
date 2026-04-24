695a0f2f23bce99243e1165623d88b529f2e4db8
sglenn211978 <sean.glenn21@gmail.com>
2026-04-24 17:45:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE = 'strawberry-patch-v1';
-const ASSETS = ['/', '/index.html'];
-
+const CACHE = 'strawberry-patch-v2';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', 'index.html', 'manifest.json'])));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
